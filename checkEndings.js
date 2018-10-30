a54a3917bd2b86d37b3b4a19d5b3f1fe0230eb16
Alex Sazonov <karl93rus@gmail.com>
2018-10-30 21:51:35 +0300

    Seems it is working. Ready for testing

---
+++ checkEndings.js
@@ -0,0 +1,48 @@
+function regcheck(city) {
+  if(/а$/.test(city) || /ль$/.test(city)) {
+    let res = '';
+    let tmp = [];
+    tmp = city.split('');
+    tmp.splice(city.length - 1, 1, 'е');
+    res = tmp.join('');
+    return res;
+  } else if(/р$/.test(city) || /ск$/.test(city) || /д$/.test(city) 
+            || /ж$/.test(city) || /ов$/.test(city) || /ан$/.test(city) || /цк$/.test(city)
+            || /рг$/.test(city) || /ул$/.test(city) || /ток$/.test(city) || /оп$/.test(city)) {
+    let res = city;
+    res += 'е';
+    return res;
+  } else if(/рь$/.test(city) || /мь$/.test(city) || /ия$/.test(city)) {
+    let res = '';
+    let tmp = [];
+    tmp = city.split('');
+    tmp.splice(city.length - 1, 1, 'и');
+    res = tmp.join('');
+    return res;
+  } else if(/ний$/.test(city)) {
+    let res = '';
+    let tmp = [];
+    tmp = city.split('');
+    tmp.splice(city.length - 2, 2, 'ем');
+    res = tmp.join('');
+    return res;
+  } else if(/кий$/.test(city) || /ый$/.test(city)) {
+    let res = '';
+    let tmp = [];
+    tmp = city.split('');
+    tmp.splice(city.length - 2, 2, 'ом');
+    res = tmp.join('');
+    return res;
+  } if(/жок$/.test(city)) {
+    let res = '';
+    let tmp = [];
+    tmp = city.split('');
+    tmp.splice(city.length - 2, 2, 'ке');
+    res = tmp.join('');
+    return res;
+  } else {
+    return city;
+  }
+}
+
+module.exports.convert = regcheck;
--- index.js
+++ index.js
@@ -1,59 +1,18 @@
-let cityArr = [
-  'Москва', 'Анапа', 'Краснодар', 'Челябинск',
-  'Новгород', 'Армавир', 'Калининград', 
-  'Воронеж', 'Иваново',  'Киров', 'Чита',
-  'Курган', 'Липецк', 'Магадан', 'Тверь', 
-  'Санкт-Петербург', 'Ярославль', 'Барнаул', 
-  'Пермь', 'Владивосток', 'Майкоп', 'Грозный',
-  'Петропавловск-Камчатский', 'Орел', 'Верхний', 
-  'Волжский', 'Россия', 
-]
+const converter = require('./checkEndings');
 
-
-function regcheck(city) {
-  if(/а$/.test(city) || /ль$/.test(city)) {
-    let res = '';
-    let tmp = [];
-    tmp = city.split('');
-    tmp.splice(city.length - 1, 1, 'е');
-    res = tmp.join('');
-    return res;
-  } else if(/р$/.test(city) || /ск$/.test(city) || /д$/.test(city) 
-            || /ж$/.test(city) || /ов$/.test(city) || /ан$/.test(city) || /цк$/.test(city)
-            || /рг$/.test(city) || /ул$/.test(city) || /ток$/.test(city) || /оп$/.test(city)) {
-    let res = city;
-    res += 'е';
-    return res;
-  } else if(/рь$/.test(city) || /мь$/.test(city) || /ия$/.test(city)) {
-    let res = '';
-    let tmp = [];
-    tmp = city.split('');
-    tmp.splice(city.length - 1, 1, 'и');
-    res = tmp.join('');
-    return res;
-  } else if(/ний$/.test(city)) {
-    let res = '';
-    let tmp = [];
-    tmp = city.split('');
-    tmp.splice(city.length - 2, 2, 'ем');
-    res = tmp.join('');
-    return res;
-  } else if(/кий$/.test(city) || /ый$/.test(city)) {
-    let res = '';
-    let tmp = [];
-    tmp = city.split('');
-    tmp.splice(city.length - 2, 2, 'ом');
-    res = tmp.join('');
-    return res;
+function cityDeclension(city) {
+  const moreThanOneWord = /[А-Яа-я]\s[А-Яа-я]/u;
+  if(moreThanOneWord.test(city)) {
+    let cityArray = city.split(' ');
+    let result = '';
+    for(let component of cityArray) {
+      result += converter.convert(component);
+      result += ' ';
+    }
+    return result;
   } else {
-    return city;
+    return converter.convert(city);
   }
 }
 
-function cityDeclension(city) {
-  
-}
-
-for(let gorod of cityArr) {
-  console.log(regcheck(gorod));
-}
+module.exports.incline = cityDeclension;
