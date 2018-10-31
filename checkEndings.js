eb79db1d5a027c974d35a9cda56a6f7190c82b03
Alex Sazonov <karl93rus@gmail.com>
2018-10-31 15:43:37 +0300

    10 more word endings to improve convertion

--- checkEndings.js
+++ checkEndings.js
@@ -1,14 +1,17 @@
 function regcheck(city) {
-  if(/а$/.test(city) || /ль$/.test(city)) {
+  if(/а$/.test(city) || /ль$/.test(city) || /ня$/.test(city)) {
     let res = '';
     let tmp = [];
     tmp = city.split('');
     tmp.splice(city.length - 1, 1, 'е');
     res = tmp.join('');
     return res;
-  } else if(/р$/.test(city) || /ск$/.test(city) || /д$/.test(city) 
+  } else if(/р$/.test(city) || /ск$/.test(city) || /д$/.test(city) || /ш$/.test(city) 
             || /ж$/.test(city) || /ов$/.test(city) || /ан$/.test(city) || /цк$/.test(city)
-            || /рг$/.test(city) || /ул$/.test(city) || /ток$/.test(city) || /оп$/.test(city)) {
+            || /рг$/.test(city) || /ул$/.test(city) || /ток$/.test(city) 
+            || /оп$/.test(city) || /с$/.test(city) || /т$/.test(city)
+            || /еп$/.test(city) || /ин$/.test(city) || /ик$/.test(city)
+            || /ен$/.test(city) || /ол$/.test(city)) {
     let res = city;
     res += 'е';
     return res;
@@ -26,7 +29,7 @@ function regcheck(city) {
     tmp.splice(city.length - 2, 2, 'ем');
     res = tmp.join('');
     return res;
-  } else if(/кий$/.test(city) || /ый$/.test(city)) {
+  } else if(/кий$/.test(city) || /ый$/.test(city) || /ое$/.test(city)) {
     let res = '';
     let tmp = [];
     tmp = city.split('');
@@ -52,6 +55,13 @@ function regcheck(city) {
     let tmp = [];
     tmp = city.split('');
     tmp.splice(city.length - 2, 2, 'ле');
+    res = tmp.join('');
+    return res;
+  } else if(/ы$/.test(city)) {
+    let res = '';
+    let tmp = [];
+    tmp = city.split('');
+    tmp.splice(city.length - 2, 2, 'ах');
     res = tmp.join('');
     return res;
   } else {
